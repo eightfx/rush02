30cb40e0d13f14fc2c43948a2dd07c91e4ed36a1
Eito Okoshi <eokoshi@c1r12s7.42tokyo.jp>
2023-08-27 15:29:17 +0900

    [doc]:

--- ex00/decomposit_num.c
+++ ex00/decomposit_num.c
@@ -6,7 +6,7 @@
 /*   By: eokoshi <eokoshi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 14:50:06 by eokoshi           #+#    #+#             */
-/*   Updated: 2023/08/27 15:24:21 by eokoshi          ###   ########.fr       */
+/*   Updated: 2023/08/27 15:29:12 by eokoshi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -24,6 +24,12 @@ int			*insert(int *list, int size, int i, int num);
 
 int			is_in_dict(t_dictionary dict, int num);
 
+// Finds the largest key in the dictionary that is less than or equal to a given number.
+// args:
+//   - dict : The dictionary containing the keys.
+//   - num : The given number.
+// returns:
+//   - int : The largest key that is less than or equal to the given number.
 int	find_max_key(t_dictionary dict, int num)
 {
 	int	i;
@@ -37,6 +43,13 @@ int	find_max_key(t_dictionary dict, int num)
 	return (max_key);
 }
 
+// Checks whether all numbers in the list exist in the dictionary.
+// args:
+//   - dict : The dictionary containing the keys.
+//   - list : The list of integers to check.
+//   - list_size : The size of the list.
+// returns:
+//   - int : 1 if all numbers are in the dictionary, 0 otherwise.
 int	check_all_in_dict(t_dictionary dict, int *list, int list_size)
 {
 	int	j;
@@ -53,6 +66,13 @@ int	check_all_in_dict(t_dictionary dict, int *list, int list_size)
 	return (1);
 }
 
+// Decomposes a number into smaller numbers based on the maximum keys in the dictionary,
+// and inserts those smaller numbers into the list.
+// args:
+//   - dict : The dictionary containing the keys.
+//   - list : A pointer to the list of integers.
+//   - list_size : A pointer to the size of the list.
+//   - i : The index of the number in the list to decompose.
 void	decompose_and_insert(t_dictionary dict, int **list, int *list_size,
 		int i)
 {
@@ -81,6 +101,12 @@ void	decompose_and_insert(t_dictionary dict, int **list, int *list_size,
 	}
 }
 
+// Processes the list to ensure all numbers in it exist in the dictionary,
+// decomposing and inserting numbers as necessary.
+// args:
+//   - dict : The dictionary containing the keys.
+//   - list : A pointer to the list of integers.
+//   - list_size : A pointer to the size of the list.
 void	process_list(t_dictionary dict, int **list, int *list_size)
 {
 	int	i;
@@ -100,6 +126,12 @@ void	process_list(t_dictionary dict, int **list, int *list_size)
 	}
 }
 
+// Decomposes a number into a list of smaller numbers that exist in the dictionary.
+// args:
+//   - dict : The dictionary containing the keys.
+//   - num : The number to decompose.
+// returns:
+//   - int* : A pointer to the list of decomposed numbers.
 int	*decomposit_num(t_dictionary dict, int num)
 {
 	int	*list;
